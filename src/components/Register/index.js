9cf7aced7a011f0f2cf9a3faf6548c3aa6508d33
Chandrakanth <chandrakanthkokkiripati1998@gmail.com>
2024-06-26 12:18:48 +0530

    c

---
+++ src/components/Register/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Register = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center'>
+      <div className='rounded p-3'>
+        <h1>Register</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default Register
