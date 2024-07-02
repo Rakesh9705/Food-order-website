aac9c9e8c31098143f828eaed212c82b092e277d
Rakesh9705 <rakeshdanaboinatpc@gmail.com>
2024-07-02 11:25:06 +0530

    Routing path issues are solved

--- src/App.js
+++ src/App.js
@@ -7,32 +7,16 @@ import ContactUs from './components/LoginForm'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+  return(
       <BrowserRouter>
       <Routes>
-        <Route exact path="/login" component={LoginForm} />
-        <Route path="/" component={HomePage} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/contact" component={ContactUs} />
+        <Route exact path="/login" element={<LoginForm />} />
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<ContactUs />} />
       </Routes>
       </BrowserRouter>
-    </div>
-  );
+  )
 }
 
 export default App;
